4a962b58952546669457c38ee2c32456e09bac1b
Peter Crowther <petercrowther90@hotmail.com>
2017-12-04 12:48:09 +0000

    refactored week 2 to be inside a function

--- week_3/example.rb
+++ week_3/example.rb
@@ -1,10 +1,19 @@
 
-
-
-def sortAlphabeticaly()
-arr = ['toby', 'thomas', 'jerry', 'mark', 'martin', 'martha']
-result = arr.sort! { |a, b|  a <=> b}
+def arrayCount()
+my_array = ["apples", "bananas", "grapes"]
+result = my_array.count()
 puts result
 end
 
-puts sortAlphabeticaly
+
+
+
+
+
+# def sortAlphabeticaly()
+# arr = ['toby', 'thomas', 'jerry', 'mark', 'martin', 'martha']
+# result = arr.sort! { |a, b|  a <=> b.}
+# puts result
+# end
+#
+# puts sortAlphabeticaly
